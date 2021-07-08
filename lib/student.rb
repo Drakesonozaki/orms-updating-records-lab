f47679be275e77a43fd681651b5b4185bd6b6b06
Drakesonozaki <josephortizbw@gmail.com>
2021-07-08 16:34:37 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,7 @@ class Student
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     #binding.pry
-    DB["conn"].execute(sql, self.name, self.grade, self.id)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
